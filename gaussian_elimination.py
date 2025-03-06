05c4f213d2908d1d1d43f7ed70f11951d011c5de
BarraHarrison <143306095+BarraHarrison@users.noreply.github.com>
2025-03-06 20:56:08 +0900

    A represents a set of Linear Equations which are then solved through the function

--- gaussian_elimination.py
+++ gaussian_elimination.py
@@ -7,3 +7,14 @@ def solve_system_of_equations(A, B):
         return solution
     except np.linalg.LinAlgError:
         return "System does not have a solution"
+    
+A = np.array([
+    [3, -1, -1],
+    [1, 1, 0],
+    [2, 0, -3]
+])
+
+B = np.array([5, 0, 2])
+
+print(solve_system_of_equations(A, B))
+# Result: [ 1.3 -1.3  0.2]
